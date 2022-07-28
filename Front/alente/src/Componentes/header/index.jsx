3ef9a83d765e4b05e4dff8ad5f946d2563895e2a
andrecosta8224 <andrecosta8224@gmail.com>
2022-07-28 15:02:52 -0300

    update

--- Front/alente/src/Componentes/header/index.jsx
+++ Front/alente/src/Componentes/header/index.jsx
@@ -1,166 +1,84 @@
-import logoalente from "../../Imagens/logoalente.png";
-import seio from "../../Imagens/seio.png";
-import "./index.css";
+import React from "react";
+import {Link} from 'react-router-dom';
+import DrawerComp from "./Drawer";
+import logoalente from "../../Imagens/logoalente.png"
+import seio from "../../Imagens/seio.png"
 
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import {
+  AppBar, 
+  Toolbar, 
+  Typography,
+  Button,
+  Tab,
+  Tabs,
+  useMediaQuery,
+  useTheme,
+  Menu,
+  MenuItem,
+  MenuIcon,
+  IconButton
+} from '@mui/material';
 
-const pages = ['Products', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+import AccountCircle from "@mui/icons-material/AccountCircle";
+
 
 const Cabecalho = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+  const pages = ["Sobre nós", "Como funciona", "Programas", "Fale conosco"];
+  const destination = ["sobrenos", "comofunciona", "programas", "faleconosco"];
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+  const isMatch = useMediaQuery(useTheme().breakpoints.down('md'));
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
+  //-----------
+
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
   };
 
-  return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
-};
+
+  return(    
+    <>
+      <AppBar position="static" sx={{background: "white", color: "black"}}>
+	<Toolbar>
+	  {
+	    isMatch ? (
+		<>
+		  <Typography sx={{display: "flex", alignItems: "center"}}><img src={logoalente} alt="logo alente" style={{width: "30%", height: "20%"}}/><img src={seio} style={{width: "20%", height: "14%"}} alt="logo seio"/></Typography>
+		  <DrawerComp/>
+		</>
+	    ) : (
+		<>
+		  <img src={logoalente} alt="logo instituto alente" style={{height: "5%", width: "15%"}}/><img src={seio} alt="logo seio" style={{width: "90px", height: "80px"}}/>
+		  <Tabs sx={{marginRight: "auto"}}>
+		  {
+		    pages.map((page, index) => (<Link to={destination[index]} style={{color: 'black', textDecoration: 'none'}}><Tab label={page}/></Link>))
+		  }
+		  </Tabs>
+		  <IconButton size="large" aria-label="account" aria-controls="menu-appbar" aria-haspopup="true" onClick={handleMenu}>
+		    <AccountCircle/>
+		  </IconButton>
+		  <Menu style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}} id="menu-appbar" anchorEl={anchorEl} anchorOrigin={{vertical: "top", horizontal: "right"}} keepMounted transformOrigin={{ vertical: "top", horizontal: "right"}} open={Boolean(anchorEl)} onClose={handleClose}>
+		    <MenuItem onClick={handleClose} style={{margin: "auto", textAlign: "center", width: "90%", background: "pink"}}><Link to="/login" style={{textAlign: "center", color: "black", textDecoration: "none", marginBttom: "3px"}}>Login</Link></MenuItem>
+		    <MenuItem onClick={handleClose} style={{textAlign: "center", width: "90%", margin: "auto", background: "pink"}}><Link to="/cadastro" style={{color: "black", textAlign: "center", textDecoration: "none"}}>Cadastre-se</Link></MenuItem>
+		  </Menu>
+		</>
+	    )
+	  }
+	  </Toolbar>
+      </AppBar>
+    </>
+
+)}
+
 export default Cabecalho;
