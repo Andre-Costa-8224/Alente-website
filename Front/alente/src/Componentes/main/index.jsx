0ffd17163f7d9f4d9d467228c8d39e8de4407477
andrecosta8224 <andrecosta8224@gmail.com>
2022-07-30 12:07:22 -0300

    update

--- Front/alente/src/Componentes/main/index.jsx
+++ Front/alente/src/Componentes/main/index.jsx
@@ -1,28 +1,39 @@
+import "./index.css"
+
 const Carrossel = () => {
-  //Criar um array para armazenar imagens importadas ou links de imagens para o carousel
+
   return(
     <>
-      <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="..." class="d-block w-100" alt="..."/>
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="..."/>
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="..."/>
-          </div>
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
+	<div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <div className="d-block bebe1 alturaimg w-100"><p>JUNTOS SOMOS MAIS FORTES!<br/><span>Entenda como nós funcionamos →</span></p></div>
+    </div>
+    <div className="carousel-item">
+      <div className="d-block bebe2 alturaimg w-100"><p>PROGRAMAS DE ATENDIMENTO<br/><span>Saiba mais →</span></p></div>
+    </div>
+    <div className="carousel-item">
+      <div className="imagemcarousel">
+        <div className="d-block marcalogoseio alturaimg w-50"><p>DOAÇÃO DE LEITE<br/><span>Saiba mais →</span></p></div>
+        <div className="d-block maeamamentando w-50 alturaimg"></div>
       </div>
+    </div>
+    <div className="carousel-item">
+      <div className="imagemcarousel">
+	<div className="d-block marcalogoseio alturaimg w-50"><p>ENTRE PARA A FAMÍLIA ALENTE<br/><span>Saiba mais →</span></p></div>
+	<div className="d-block w-50 bebesegurandodedo alturaimg"></div>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
     </>
   )
 }
